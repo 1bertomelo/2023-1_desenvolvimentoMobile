d4662bf3e18efed763c21efb6b8d3375563639f3
betomelo_9 <betomelo_9@hotmail.com>
2023-02-16 18:17:29 -0300

    Exemplo navegação

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import AppForm from './AppForm';
+import React, {useState} from 'react';
 import ConceitoComponente from './ConceitoComponente';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Hello from './Pages/Hello/Hello';
+import Page1 from './Pages/Page1/Page1';
+import Page2 from './Pages/Page2/Page2';
+
+const Stack = createStackNavigator();
 
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-    <ConceitoComponente />
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen  name="Hello" component={Hello} />
+        <Stack.Screen  name="Page1" component={Page1} />
+        <Stack.Screen  name="Page2" component={Page2} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: '#000', 
     justifyContent: 'center',
   },
 });
---
+++ Pages/Page1/Page1.js
@@ -0,0 +1,78 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import React, {useState} from 'react';
+import { useNavigation } from '@react-navigation/native'
+
+export default function Page1() {
+    const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Page1</Text>
+      <View style={styles.inputContainer}> 
+         <Text style={styles.textResult}>Olá</Text>
+         <TouchableOpacity style={styles.button}  onPress={()=>navigation.goBack()} > 
+          <Text style={styles.buttonText}>Voltar</Text> 
+        </TouchableOpacity> 
+      </View>      
+    </View> 
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    backgroundColor: '#D93600',
+    alignItems: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 50,
+  },
+  inputContainer: {
+    
+    marginTop: 30,
+    width: '90%',
+    padding: 20,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    borderBottomLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    alignItems: 'stretch',
+    backgroundColor: '#fff',
+    height: '80%'
+
+  },
+  input: {
+    marginTop: 10,
+    height: 60,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    paddingHorizontal: 24,
+    fontSize: 16,
+    alignItems: 'stretch'
+  },
+  button: {
+    marginTop: 10,
+    height: 60,
+    backgroundColor: 'blue',
+    borderRadius: 10,
+    paddingHorizontal: 24,
+    fontSize: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 20,
+    shadowOpacity: 20,
+    shadowColor: '#ccc',
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  textResult: {
+    color: '#000',
+    fontWeight: 'bold',
+    textAlign : 'center'
+  }
+});
